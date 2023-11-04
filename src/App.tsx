3fc4d0e75c745f3b8356a16247a8e4ebda96e6c6
ardhikaxx <ardhikayanuar58@gmail.com>
2023-11-04 19:03:38 +0700

    first upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [otp, setOtp] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value) && value.length <= 4) {
+      setOtp(value);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>OTP Generator</h1>
+        <div className="otp-container">
+          {Array.from({ length: 4 }, (_, index) => (
+            <input
+              key={index}
+              type="text"
+              className="otp-input"
+              value={otp[index] || ''}
+              onChange={handleChange}
+            />
+          ))}
+        </div>
+        <button className="generate-button" onClick={() => setOtp(generateOtp())}>
+          Generate OTP
+        </button>
       </header>
     </div>
   );
+}
+
+function generateOtp() {
+  return Math.floor(1000 + Math.random() * 9000).toString();
 }
 
 export default App;
